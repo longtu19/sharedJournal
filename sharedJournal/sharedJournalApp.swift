8c687db591c3378f7ad1e2a3e0416bb68e7e500a
Long Huynh <longbhuynh019@gmail.com>
2025-05-30 19:47:53 -0500

    Initial Commit

---
+++ sharedJournal/sharedJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  sharedJournalApp.swift
+//  sharedJournal
+//
+//  Created by Long Huynh on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct sharedJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sharedJournalTests/sharedJournalTests.swift
@@ -0,0 +1,16 @@
+//
+//  sharedJournalTests.swift
+//  sharedJournalTests
+//
+//  Created by Long Huynh on 5/30/25.
+//
+
+import Testing
+
+struct sharedJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
